66d63e9b960a42f878e472921863aea43deea910
Ilya Sevastyanov <sevastyanov-1990@bk.ru.>
2024-02-18 18:21:51 +0300

    Выполнено первое задание(добавлены комментарии к классу PromotionalClient)

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -1,18 +1,20 @@
 package classes;
 
-import classes.Actor;
-import classes.Market;
-import classes.OrdinaryClient;
-import classes.SpecialClient;
-
 public class Main {
     public static void main(String[] args) {
         Market magnit = new Market();
         Actor client1 = new OrdinaryClient("Tolik");
         Actor client2 = new OrdinaryClient("Any");
-        Actor client3 = new SpecialClient("President",777);
-        Actor client4 = new PromotionalClient("Edward",22);
-        TaxInspector client5 = new TaxInspector();
+        Actor client3 = new SpecialClient("President", 777);
+        TaxInspector client4 = new TaxInspector();
+        PromotionalClient client5 = new PromotionalClient("Edward", 22);
+        PromotionalClient.setCountPromoClient(23);
+        client5.setNameOfAction("EVERY 2 NUTS FREE");
+        client5.showCurrentPromo();
+        client5.setNameOfAction("10%  oil");
+        PromotionalClient.setCountPromoClient(3);
+        client5.showCurrentPromo();
+
         magnit.acceptToMarket(client1);
         magnit.acceptToMarket(client2);
         magnit.acceptToMarket(client3);
@@ -21,5 +23,4 @@ public class Main {
         magnit.update();
 
     }
-
 }
--- src/main/java/classes/PromotionalClient.java
+++ src/main/java/classes/PromotionalClient.java
@@ -1,10 +1,13 @@
 package classes;
 
+/**
+ * Класс описывает структуру акционного клиента.
+ */
 public class PromotionalClient extends Actor {
 
-    private String promoOfTheDay;
-    private int idPromoClient;
-    static int countPromo;
+    private String nameOfPromo;  // название акции
+    private int idPromoClient; // ID акционного клиента
+    private static int countPromoClient; // количество участников акции
 
 
     public PromotionalClient(String name, int idPromoClient) {
@@ -12,40 +15,137 @@ public class PromotionalClient extends Actor {
         this.idPromoClient = idPromoClient;
     }
 
+
+    /**
+     * Метод получения имени акционного клиента
+     * @return возвращает имя акционного клиента
+     */
+
     @Override
     public String getName() {
         return super.name;
     }
 
+    /**
+     * Метод присвоения имени акционного клиента
+     *
+     * @param name задаваемое имя акционного клиента
+     */
     @Override
     public void setName(String name) {
-        super.name = name;
+
     }
+
+    /**
+     *
+     * Метод для получения флага состояния полученного заказа клиентом
+     * @return флаг состояния - клиент получил или не получил заказ
+     */
 
     @Override
     public boolean isTakeOrder() {
-        return  super.isTakeOrder;
+        return isTakeOrder;
     }
+
+    /**
+     * Метод для получения флага состояния сделанного заказа клиентом
+     *
+     * @return флаг состояния - клиент сделал или не сделал заказ
+     */
 
     @Override
     public boolean isMakeOrder() {
-        return  super.isMakeOrder;
+        return isMakeOrder;
     }
+
+    /**
+     * Метод для установки флага состояния полученного заказа клиентом
+     *
+     * @param isCreated переменная состояния заказа у клиента - получен или не получен
+     */
 
     @Override
     public void setTakeOrder(boolean isCreated) {
-        super.isTakeOrder = isCreated;
+        this.isTakeOrder = isCreated;
 
     }
 
+    /**
+     * Метод для установки флага состояния сделанного заказа клиентом
+     *
+     * @param isCreated переменная состояния заказа у клиента - сделан или не сделан
+     */
     @Override
     public void setMakeOrder(boolean isCreated) {
-        super.isMakeOrder = isCreated;
+        this.isMakeOrder = isCreated;
 
     }
+
+    /**
+     * Метод получения экземпляра акционного клиента
+     * @return возвращает экземпляр акционного клиента
+     */
 
     @Override
     public Actor getActor() {
         return this;
+    }
+
+    /**
+     * Метод возвращает id клиента
+     * @return возвращает id клиента
+     */
+    public int getID() {
+        return idPromoClient;
+    }
+    /**
+     * Метод устанавливает id клиента
+     * @param idPromoClient для присвоения id клиенту
+     */
+    public void setID(int idPromoClient) {
+        this.idPromoClient = idPromoClient;
+    }
+
+    /**
+     * Метод возвращает количество участников акции
+     * @return количество участников акции
+     */
+    public static int getCountPromoClient() {
+        return countPromoClient;
+    }
+
+    /**
+     * Метод задает количество участников акции
+     * @param countPromoClient количество участников акции
+     */
+    public static void setCountPromoClient(int countPromoClient) {
+        PromotionalClient.countPromoClient = countPromoClient;
+
+    }
+
+    /**
+     * Метод возвращает название акции
+     * @return название акции
+     */
+    public String getNameOfPromo() {
+        return nameOfPromo;
+    }
+
+    /**
+     * Метод задает название акции
+     * @param nameOfPromo название акции
+     */
+    public void setNameOfAction(String nameOfPromo) {
+        this.nameOfPromo = nameOfPromo;
+    }
+
+    /**
+     * Метод показывает, является ли клиент участником текущей акции
+     */
+    public void showCurrentPromo() {
+        if (idPromoClient <= countPromoClient) {
+            System.out.println("Клиент " + super.name + " с id:" + this.idPromoClient + " участвует в акции " + this.nameOfPromo);
+        } else
+            System.out.println("Клиент " + super.name + " с id:" + this.idPromoClient + "  не участвует в акции " + this.nameOfPromo);
     }
 }
